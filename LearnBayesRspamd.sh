c33d1704de2e83a2acf26b0f98abc99ded892c78
Jordi van Nistelrooij <jordivn@gmail.com>
2024-12-29 19:52:05 +0100

    Update LearnBayesRspamd.sh
    Updates to:
    - Not look at inbox (because of user who not maintain)
    - Not look at trash, temp en new
    - More spamfolders to recornize.

--- LearnBayesRspamd.sh
+++ LearnBayesRspamd.sh
@@ -1,18 +1,25 @@
+
 #!/bin/sh
 #Author: Jordi van Nistelrooij - Webs en systems
 #Website: https://websensystems.nl
 #Contact: info@websensystems.nl
-#Date: 2023-05-19
-#Version: 1.0
-#Discription: Script for leaning userbased bayes with mailbox folders other then default (ham) and know spam folders (spam). Also use mbox spam from artinvoice
-# THIS VERSION IS FOR RSPAMD used in directadmin (Socket!!!)
+#Date: 2024-12-29
+#Version: 2.0
+#Discription: Script for leaning userbased bayes with mailbox folders other then default (ham) and know spam folders>
 
 LOGPATH='/var/log/sa-learn/'
 mkdir -p $LOGPATH
 
+
+# Definieer spam-mappen
+SPAM_FOLDERS=(".Ongewenste e-mail" ".spam" ".Spam" ".Unwanted" ".Junk" ".Junk E-mail" ".Courrier indésirable" ".Unerwünschte E-Mails" ".Correo no deseado" ".Posta indesiderata" ".Junkmail" ".Spamfolder" ".Unwanted" ".Blocked" ".Blacklisted" ".BULK" ".INBOX.Spam" ".INBOX.spam")
+# Definieer uitzonderingen
+IGNORE_FOLDERS=(".Trash" "cur" "tmp" "new" ".Verwijderde items" "INBOX.Verwijderde items" ".Trash.Verwijderde items")
+
+
+
 for USER in `ls /usr/local/directadmin/data/users`;
 do
-#rm $LOGPATH$USER.log -f
 echo "=== Start ===" >> $LOGPATH$USER.log
 echo $(date +%F) >> $LOGPATH$USER.log
 echo "=============" >> $LOGPATH$USER.log
@@ -21,44 +28,43 @@ echo -e "===\t===\t===\t===\t===" >> $LOGPATH$USER.log
 
 
 
-for DOMAINS in `ls -d /home/$USER/imap/*`;
-do
+for DOMAIN in $(ls -d /home/$USER/imap/*); do
+  for MAILBOX in $(ls -d $DOMAIN/*); do
+    # Leer spam-mappen
+    for FOLDER in "${SPAM_FOLDERS[@]}"; do
+      if [ -d "$MAILBOX/Maildir/$FOLDER" ]; then
+        echo "Leren van spam uit $MAILBOX/Maildir/$FOLDER" >> $LOGPATH$USER.log
+        rspamc learn_spam "$MAILBOX/Maildir/$FOLDER" >> >> $LOGPATH$USER.log
+      fi
+    done
 
-if [ `find $DOMAINS -maxdepth 1 -type d | wc -l` -ge 2 ]
-then
-for MAILBOX in `ls -d $DOMAINS/*`;
-do
+    # Dynamisch bouwen van uitsluitingen voor spam-mappen
+    EXCLUDES=""
+    if [ "${#SPAM_FOLDERS[@]}" -gt 0 ]; then
+      for FOLDER in "${SPAM_FOLDERS[@]}"; do
+        EXCLUDES+="! -iname \"$FOLDER\" "
+      done
+    fi
 
-if [ `find $MAILBOX -maxdepth 1 -type d | wc -l` -ge 2 ]
-then
-IFS=$'\n'
-for HAMMAILBOXFOLDER in `ls -d $MAILBOX/Maildir/.* | egrep -v -i 'spam|Ongewenst|verwijderde|trash|drafts|concepten|junk|prullenbak|unwanted|deleted|\.$|\.\.$'`;
-do
-if [ -d "${HAMMAILBOXFOLDER}/cur" ]
-then
-echo -e `basename ${DOMAINS}` "\t" `basename ${MAILBOX}` "\tHAM\t" `basename ${HAMMAILBOXFOLDER}` "\t" `rspamc -h /var/run/rspamd/rspamd_controller.sock learn_ham  "${HAMMAILBOXFOLDER}/cur"` >> $LOGPATH$USER.log
-fi
+if [ "${#IGNORE_FOLDERS[@]}" -gt 0 ]; then
+      for FOLDER in "${IGNORE_FOLDERS[@]}"; do
+        EXCLUDES+="! -iname \"$FOLDER\" "
+      done
+    fi
+
+
+    # Leer ham-mappen
+    eval find "$MAILBOX/Maildir" -mindepth 1 -maxdepth 1 -type d  $EXCLUDES |
+    while read -r HAM_FOLDER; do
+      echo "Leren van ham uit $HAM_FOLDER" >> $LOGPATH$USER.log
+     rspamc learn_ham "$HAM_FOLDER" >> $LOGPATH$USER.log
+
+    done
+  done
 done
 
-IFS=$'\n'
-for SPAMMAILBOXFOLDER in `ls -d $MAILBOX/Maildir/.* | egrep -i 'spam|Ongewenst|junk|unwanted'`;
-do
-if [ -d "${SPAMMAILBOXFOLDER}/cur" ]
-then
-echo -e `basename ${DOMAINS}` "\t" `basename ${MAILBOX}` "\tSPAM\t" `basename ${SPAMMAILBOXFOLDER}` "\t" `rspamc -h /var/run/rspamd/rspamd_controller.sock learn_spam "${SPAMMAILBOXFOLDER}/cur"` >> $LOGPATH$USER.log
-fi
-done
-fi
-done
-fi
-done
 echo -e "===\t===\t===\t===\t===" >> $LOGPATH$USER.log
-#echo "Syncing" >> $LOGPATH$USER.log
-#sa-learn --sync --dbpath /home/$USER/.spamassassin >> $LOGPATH$USER.log
-#echo "Bayes info" >> $LOGPATH$USER.log
-#sa-learn --dump magic --dbpath /home/$USER/.spamassassin >> $LOGPATH$USER.log
-chown $USER:$USER /home/$USER/.spamassassin/bayes_*
-echo "=== Done ===" >> $LOGPATH$USER.log
+
 done
 echo "Info" >> /var/log/sa-learn/overall.log
 echo rspamc -h /var/run/rspamd/rspamd_controller.sock stat >> /var/log/sa-learn/overall.log
@@ -73,10 +79,9 @@ cd /etc/mail/spamassassin >> /var/log/sa-learn/overall.log
 /usr/bin/wget -N https://dutchspamassassinrules.nl/DSR/DSR.cf >> /var/log/sa-learn/overall.log
 /usr/bin/wget -N https://www.pccc.com/downloads/SpamAssassin/contrib/KAM.cf >> /var/log/sa-learn/overall.log
 echo "Downloaded DSR & KAM" >> /var/log/sa-learn/overall.log
-echo "Restarting RSPAMD" >> /var/log/sa-learn/overall.log
-systemctl restart rspamd >> /var/log/sa-learn/overall.log
+echo "Restarting spamassassin" >> /var/log/sa-learn/overall.log
+systemctl restart spamassassin >> /var/log/sa-learn/overall.log
 echo "DSR & KAM update done" >> /var/log/sa-learn/overall.log
 rm -f $spamfile
 rm -fr $spamfile_unpacked
-
 exit
